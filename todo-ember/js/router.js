68cf0b366baf791cb25738c8da5419cf10575a08
Iker Arbulu Lozano <ikerarbulu@gmail.com>
2016-02-13 14:37:54 -0600

    Already using alternative routes with filters on the list

--- todo-ember/js/router.js
+++ todo-ember/js/router.js
@@ -1,4 +1,8 @@
 Todos.Router.map(function(){
-  this.resource('todos',{path: '/'}); //Maneja un recurso que puede tener hijos (nested)
+  this.resource('todos',{path: '/'},function(){
+    //additional child routes will go here later
+    this.route('active');
+    this.route('completed');
+  }); //Maneja un recurso que puede tener hijos (nested)
   this.route('my_route',{path: '/my_route'}); //Maneja la redirección simple (get normalito)
-})
+});
